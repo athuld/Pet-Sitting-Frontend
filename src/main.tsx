ad319f359988374da24189ab73435e9999caf475
Athul Dinesan <athuldinesan5@gmail.com>
2022-10-14 02:54:25 +0530

    frontend:[temp]: remove react query dev tools
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +18,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <NotificationsProvider position="bottom-right">
         <QueryClientProvider client={queryClient}>
           <App />
-          <ReactQueryDevtools initialIsOpen />
         </QueryClientProvider>
       </NotificationsProvider>
     </MantineProvider>
